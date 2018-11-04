a381bb3c4469eb3c73e1cf502990502e1d3236d0
Tevin Jeffrey <tev.jeffrey@gmail.com>
2018-11-03 22:32:27 -0400

    More error handling (#2)

--- main.go
+++ main.go
@@ -8,40 +8,73 @@ import (
 	"strings"
 	"time"
 
-	"github.com/pkg/errors"
 	log "github.com/sirupsen/logrus"
 	"github.com/urfave/cli"
 	"gopkg.in/andygrunwald/go-jira.v1"
 )
 
+var Version string
+var Commit string
+var Date string
+
+type Transitions string
+
+const (
+	REVIEW_STATUS     Transitions = "In (Code) Review"
+	IN_PROGESS_STATUS Transitions = "In Progress"
+	READY_FOR_QA      Transitions = "Ready for QA"
+)
+
+func (t Transitions) String() string {
+	return string(t)
+}
+
+var token = ""
+var baseUrl = ""
+var username = ""
+var dryRun = false
+
 func main() {
+	log.SetFormatter(&log.TextFormatter{
+		DisableTimestamp: true,
+		DisableColors:    true,
+	})
+
 	app := cli.NewApp()
 	app.Name = "git-jira"
-	app.Version = "0.1.0"
+	app.Version = Version + " " + Commit
 	app.Compiled = time.Now()
-	app.UsageText = "git jira #ticket [global options]"
-	app.Usage = "Perform jira operations for a git repo"
+	app.UsageText = "git jira #issue [global options]"
 
 	app.Flags = []cli.Flag{
 		cli.StringFlag{
-			Name:   "username, u",
-			EnvVar: "JIRA_USERNAME",
-			Usage:  "Your Jira username, usually you email.",
+			Name:        "username, u",
+			EnvVar:      "JIRA_USERNAME",
+			Usage:       "Your Jira username, usually your email.",
+			Destination: &username,
 		},
 		cli.StringFlag{
-			Name:   "token, t",
-			EnvVar: "JIRA_API_TOKEN",
-			Usage:  "API token created on https://id.atlassian.com/manage/api-tokens",
+			Name:        "token, t",
+			EnvVar:      "JIRA_API_TOKEN",
+			Usage:       "An API token created on https://id.atlassian.com/manage/api-tokens",
+			Destination: &token,
 		},
 		cli.StringFlag{
-			Name:   "base-url, b",
-			EnvVar: "JIRA_BASE_URL",
-			Usage:  "e.g mycompany.atlassian.com",
+			Name:        "base-url, b",
+			EnvVar:      "JIRA_BASE_URL",
+			Usage:       "e.g mycompany.atlassian.com",
+			Destination: &baseUrl,
+		},
+		cli.BoolFlag{
+			Name:        "dry-run, d",
+			Usage:       "Do not run any git commands or modify any resource via Jira API.",
+			Destination: &dryRun,
 		},
 	}
 
+
 	app.Action = func(c *cli.Context) error {
-		err := dostuff(c)
+		err := GitJira(c)
 		if err != nil {
 			log.Fatal(err.Error())
 		}
@@ -54,21 +87,36 @@ func main() {
 var issueIdRegex, _ = regexp.Compile(`\S+-\d+`)
 var issueTitleRegex, _ = regexp.Compile("[^a-zA-Z0-9]+")
 
-func dostuff(context *cli.Context) error {
-	username := context.String("username")
-	token := context.String("token")
-	baseUrl := context.String("base-url")
-	issueRaw := context.Args().First()
-
+func pareseIssueId(ctx *cli.Context) string {
+	issueRaw := ctx.Args().First()
 	splits := strings.Split(issueRaw, "/")
 	issueID := splits[len(splits)-1]
 
-	if issueID == "" {
-		errors.New("The supplied issue is empty")
+	if strings.TrimSpace(issueID) == "" {
+		cli.ShowAppHelp(ctx)
+		log.Exit(1)
 	}
 
 	if !issueIdRegex.MatchString(issueID) {
-		errors.New(fmt.Sprintf("Incorrect issue format: %s", issueID))
+		exitFatal(issueID, nil, fmt.Sprintf("Incorrect issue format: %s", issueID))
+	}
+
+	return issueID
+}
+
+func GitJira(context *cli.Context) error {
+	issueID := pareseIssueId(context)
+
+	if username == "" {
+		exitFatal(issueID, nil, "--username or JIRA_USERNAME was empty")
+	}
+
+	if token == "" {
+		exitFatal(issueID, nil, "--token or JIRA_API_TOKEN was empty")
+	}
+
+	if baseUrl == "" {
+		exitFatal(issueID, nil, "--base-url or JIRA_BASE_URL was empty")
 	}
 
 	tp := jira.BasicAuthTransport{
@@ -78,10 +126,14 @@ func dostuff(context *cli.Context) error {
 
 	client, err := jira.NewClient(tp.Client(), "https://"+baseUrl)
 	if err != nil {
-		return err
+		exitFatal(issueID, err, "error while creating Jira http client")
 	}
 
+	// 1.  Create branch from issue nam,e
 	issue, _, err := client.Issue.Get(issueID, &jira.GetQueryOptions{Fields: "summary"})
+	if err != nil {
+		exitFatal(issueID, err, "error while getting Jira issue")
+	}
 
 	var issueType = getIssueType(issue.Fields.Type)
 	var branchName = createBranchName(issue.Fields.Summary)
@@ -90,11 +142,33 @@ func dostuff(context *cli.Context) error {
 
 	gitCheckout(gitBranchName)
 
+	// 2. Create an empty first commit in the new branch
 	var commitHeader = fmt.Sprintf("[%s] %s", issueID, issue.Fields.Summary)
 
-	gitCommit(commitHeader, "\t", issue.Self)
+	var issueLink = "https://" + baseUrl + "/browse/" + issueID
 
-	// TODO transition ticket to in-progress
+	gitCommit(commitHeader, "\t", issueLink)
+
+	// 3. Set the issue to in progress
+	transitions, _, err := client.Issue.GetTransitions(issueID)
+	if err != nil {
+		exitFatal(issueID, err, "error while getting all transitions")
+	}
+
+	for _, v := range transitions {
+		if v.Name == IN_PROGESS_STATUS.String() {
+			if dryRun {
+				log.Println("dry-run: transition issue to 'In-Progress")
+				break
+			}
+			_, err := client.Issue.DoTransition(issueID, v.ID)
+			if err != nil {
+				exitFatal(issueID, err, "error while transitioning issue to 'In-Progress' state")
+			}
+			break
+		}
+	}
+
 	return nil
 }
 
@@ -150,6 +224,11 @@ func gitCommit(message ...string) {
 }
 
 func executeCommand(command []string) *exec.Cmd {
+	if dryRun {
+		log.Println("dry-run: " + strings.Join(command, " "))
+		return nil
+	}
+
 	cmd := execCommand(command[0], command[1:]...)
 	cmd.Stderr = os.Stderr
 	cmd.Stdout = os.Stdout
@@ -183,5 +262,15 @@ func createBranchName(issueTitle string) string {
 		return r
 	}, output)
 
+	output = strings.Trim(output, "_")
+
+	if len(output) > 60 {
+		output = output[:60]
+	}
+
 	return strings.Trim(output, "_")
+}
+
+func exitFatal(issueID string, err error, message string) {
+	log.WithError(err).WithField("issueID", issueID).Fatalln(message)
 }
